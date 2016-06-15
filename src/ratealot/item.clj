738aa8b5c0e978306fd3664486773e5425f5b27d
Stephan van Ellewee <stephan.vanellewee@takealot.com>
2016-06-15 13:13:19 +0200

    itemgetter

--- src/ratealot/item.clj
+++ src/ratealot/item.clj
@@ -21,24 +21,40 @@
   ;;   (car/get "foo")) ; => ["PONG" "OK" "bar"] (3 commands -> 3 replies)
   )
 
+(def make-item-key (partial str "items:"))
+(defn make-key-from-item [item]
+  (let [ barcode (:barcode item) ]
+    (make-item-key barcode)))
+
 (defn get-item
   "Get an item by barcode. If it doesn't exist, return nil"
   [barcode]
-  (:barcode @items barcode))
+  ;;(:barcode @items barcode))
+  (let [ item-key (make-item-key barcode)
+         key-getter (partial car/hmget item-key)
+        item-names (map name (keys Item) )
+        item-value (wcar* (apply key-getter item-names))
+        [barcode name description]  item-value ]
+    {:barcode barcode :name name :description description}))
 
 (defn add! [new-item]
   (let [item (coerce! Item new-item)
-        barcode (:barcode new-item)]
-    (wcar* (car/hmset (str "items:" barcode)
-                      "barcode" (:barcode new-item)
-                      "name" (:name new-item)
-                      "description" (:description new-item)))
-    ))
+        barcode (:barcode new-item)
+        description (:description new-item)
+        name (:name new-item)
+        item-key (make-key-from-item new-item) ]
+    (wcar* (car/hmset item-key
+                      "barcode" barcode
+                      "name" name
+                      "description" description)) ))
+    
 
 (comment 
   (when (empty? @items)
-    (add! {:barcode "abc123" :name "Camel Filteragain" :description "Toasted lekker cigs" :reviews '[]})
-    (add! {:barcode "abc122" :name "Camel Filter STUFF" :description "Toasted lekker cigs" :reviews '[]})
-    (add! {:barcode "abc125" :name "TEXAN" :description "Toasted lekker cigs" :reviews '[]}))
+    (add! {:barcode "abc123" :name "Camel Filteragain" :description "Toasted lekker cigs" :reviews [] })
+    (add! {:barcode "abc122" :name "Camel Filter STUFF" :description "Toasted lekker cigs" :reviews []})
+    (add! {:barcode "abc125" :name "TEXAN" :description "Toasted lekker cigs ALSO sssYADAD" :reviews []}))
+  (get-item "abc125")
+(keys Item)
   
   )
