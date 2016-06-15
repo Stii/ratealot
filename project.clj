6e1ca11e921a6fe8833e9c90782d4e9af62243be
Stii <stii@takealot.com>
2016-06-15 09:09:23 +0200

    Added the carmine for redis requirement

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-time "0.11.0"] ; required due to bug in `lein-ring uberwar`
-                 [metosin/compojure-api "1.1.3"]]
+                 [metosin/compojure-api "1.1.3"]
+                 [com.taoensso/carmine "2.13.0"]]
   :ring {:handler ratealot.handler/app}
   :uberjar-name "server.jar"
   :uberwar-name "server.war"
